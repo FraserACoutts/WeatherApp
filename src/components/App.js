e1802f0b9282056fd21e26209bc7d8f9978b141f
Fraser Coutts <fcouttsdev@gmail.com>
2020-05-29 14:53:13 +0100

    using async await to get data back from api call

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,25 @@ import axios from 'axios';
 import Search from './Search';
 
 class App extends React.Component {
-    state = { location: '' };
+    state = { weather: [] };
 
-    onSearchSubmit(term) {
-        axios.get('http://api.weatherapi.com/v1/forecast.json', {
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('http://api.weatherapi.com/v1/forecast.json', {
             params: {
                 key: 'cfb5e6d9acd0434f874112648202805',
                 query: term,
                 days: 3
             },
-
         });
-    }
+
+        this.setState({ weather: response.data.forecast.forecastday });
+    };
 
     render() {
         return (
             <div>
                 <Search onSubmit={this.onSearchSubmit} />
+                Found: {this.state.weather.length}
             </div>
         );
     }
