f4fa80916aa0311b3f55a0ea8940a475616a603e
Fraser Coutts <fcouttsdev@gmail.com>
2020-06-01 16:06:02 +0100

    added moment to the app to input the date on rendering the application

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import axios from 'axios';
+import moment from 'moment';
 import Search from './Search';
 import CurrentTemp from './CurrentTemp';
 
 class App extends React.Component {
-    state = { forecasts: [], location: [], current: [] };
+    state = { forecasts: [], location: [], current: [], date: [] };
 
     onSearchSubmit = async (term) => {
         const response = await axios.get('http://api.weatherapi.com/v1/forecast.json', {
@@ -18,7 +19,8 @@ class App extends React.Component {
         this.setState({
             forecasts: response.data.forecast.forecastday,
             location: response.data.location,
-            current: response.data.current
+            current: response.data.current,
+            date: moment().format('MMMM Do')
         });
     };
 
@@ -26,7 +28,7 @@ class App extends React.Component {
         return (
             <div>
                 <Search onSubmit={this.onSearchSubmit} />
-                <CurrentTemp current={this.state.current} location={this.state.location} forecasts={this.state.forecasts} />
+                <CurrentTemp current={this.state.current} location={this.state.location} forecasts={this.state.forecasts} date={this.state.date} />
             </div>
         );
     }
--- src/components/CurrentTemp.js
+++ src/components/CurrentTemp.js
@@ -1,19 +1,29 @@
 import React from 'react';
 
-const CurrentTemp = ({ current, location, forecasts }) => {
+const CurrentTemp = ({ current, location, forecasts, date }) => {
     console.log(current);
     console.log(location);
     console.log(forecasts);
 
     const image = forecasts.map((forecast) => {
-        return <img src={forecast.day.condition.icon} />;
+        return (
+            <div>
+                <img alt={forecast.text} key={forecast.date_epoch} src={forecast.day.condition.icon} />
+                <p>{forecast.day.condition.text}</p>
+            </div>
+        );
     });
 
     return (
         <div>
-            <h1>{location.name} {location.country}</h1>
-            <p>{current.temp_c}</p>
-            {image[0]}
+            <div>
+                <h1>{location.name} {location.country}</h1>
+                {date}
+            </div>
+            <div>
+                {image[0]}
+                <p>Current Temp {current.temp_c}</p>
+            </div>
         </div>
     );
 };
